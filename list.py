9eff1deda1c99c6e4ffb863298a40841edd1c3bc
zhangkuilong <uestcklz_86@163.com>
2017-11-01 14:34:58 +0800

    Create list.py

---
+++ list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+shoplist = ['apple', 'orange', 'banana']
+print(shoplist)
+print(shoplist[0])
+print(shoplist[1:3])
+print(shoplist[1:-1])
+
+shoplist.append('aaa')
+print(shoplist)
+print(shoplist[-1])
